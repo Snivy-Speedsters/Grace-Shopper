e311f95bdfdb53c789b445d5d6e7b61cab017890
joshua constine <joshua.constine97@gmail.com>
2022-03-01 14:17:05 -0600

    added all products reducer, added /products route

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,17 +1,46 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchProducts } from "../store/products";
 
 class AllProducts extends Component {
-  constructor(props) {
-    super(props);
+  componentDidMount() {
+    try {
+      this.props.getProducts();
+    } catch (error) {
+      console.error(error);
+    }
   }
-  state = {};
+
   render() {
+    let products = this.props.products || [];
     return (
       <div>
-        <h1>hello world</h1>
+        <h1>all products</h1>
+        <ul>
+          {products.map((product) => {
+            return (
+              <div product={product} key={product.id}>
+                <h2>{product.name}</h2>
+                <h2>{product.price}</h2>
+              </div>
+            );
+          })}
+        </ul>
       </div>
     );
   }
 }
 
-export default AllProducts;
+const mapState = (state) => {
+  return {
+    products: state.products,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    getProducts: () => dispatch(fetchProducts()),
+  };
+};
+
+export default connect(mapState, mapDispatch)(AllProducts);
---
+++ client/store/products.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const initialState = [];
+
+//action constants
+
+const SET_PRODUCTS = "SET_PRODUCTS";
+
+//action creators
+
+export const setProducts = (product) => {
+  return {
+    type: SET_PRODUCTS,
+    product,
+  };
+};
+//thunk creators
+export const fetchProducts = () => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get("/api/product");
+      dispatch(setProducts(data));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+//reducer
+
+export default function productsReducer(products = initialState, action) {
+  switch (action.type) {
+    case SET_PRODUCTS:
+      return action.product;
+
+    default:
+      return products;
+  }
+}
