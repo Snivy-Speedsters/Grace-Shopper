8490d3171141fa395bf67c767a2d461beacd656b
joshua constine <joshua.constine97@gmail.com>
2022-03-01 11:37:42 -0600

    added all products component

---
+++ client/components/AllProducts.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class AllProducts extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {};
+  render() {
+    return (
+      <div>
+        <h1>hello world</h1>
+      </div>
+    );
+  }
+}
+
+export default AllProducts;
