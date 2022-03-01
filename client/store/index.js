dd230b7bd4f6223800897aec83554438b353d06d
Andrew Ruesga <andrewruesga@icloud.com>
2022-03-01 16:16:52 -0600

    Single Product Vertical Slice Added: Backend Route, Frontend Component, Frontend Store. Clickability between All Products-Single Product

--- client/store/index.js
+++ client/store/index.js
@@ -1,18 +1,20 @@
-import { createStore, combineReducers, applyMiddleware } from "redux";
-import { createLogger } from "redux-logger";
-import thunkMiddleware from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
-import auth from "./auth";
-import products from "./products";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import auth from './auth';
+import products from './products';
+import product from './singleProduct';
 
 const reducer = combineReducers({
-  products,
-  auth,
+	products,
+	product,
+	auth,
 });
 const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
+	applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
 );
 const store = createStore(reducer, middleware);
 
 export default store;
-export * from "./auth";
+export * from './auth';
