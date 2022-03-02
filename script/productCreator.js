2fdc3eb788496850fa37593dafb82d6350578347
Robert Brown <robert.t.brown352@gmail.com>
2022-03-01 21:20:45 -0500

    Fixed the image url and gendered name generation.

--- script/productCreator.js
+++ script/productCreator.js
@@ -1,15 +1,14 @@
 const { faker } = require('@faker-js/faker')
 
 function createProduct(index){
-  const gender = faker.name.gender(true)
-  const name = faker.name.findName(undefined, undefined, gender)
+  const gender = faker.name.gender(true).toLowerCase()
+  const name = faker.name.findName(undefined, undefined, gender === 'male' ? 0 : 1)
   const price = faker.commerce.price(1000, 5000)
   const sku = faker.random.alphaNumeric(10)
-  const imageUrl = `${gender}/${index}`
+  const imageUrl = `/images/${gender}/${gender}${index}.jpg`
 
 
   return{
-    gender,
     name,
     price,
     sku,
