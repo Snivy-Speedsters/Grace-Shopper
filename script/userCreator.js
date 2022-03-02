574fbfc8c0329733dc76de14cf4eca6a755c223d
Robert Brown <robert.t.brown352@gmail.com>
2022-03-01 21:02:37 -0500

    Commented out the random password generator in userCreator.js and hardcoded it as 123

--- script/userCreator.js
+++ script/userCreator.js
@@ -5,7 +5,8 @@ function createUser(){
     const firstName = faker.name.firstName()
     const lastName = faker.name.lastName()
     const email = faker.internet.email(firstName, lastName)
-    const password = faker.internet.password(10)
+    // const password = faker.internet.password(10)
+    const password = '123'
     const shippingAddress = faker.address.streetName()
 
 
