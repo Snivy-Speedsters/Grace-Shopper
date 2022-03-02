cfc5125dcdc8f9245912afc7fd346c9e8a4e45e5
AndrewRuesga <95383524+AndrewRuesga@users.noreply.github.com>
2022-03-02 16:45:11 -0600

    Hotfix merge issues for cart checkout and add to cart function

--- client/components/CartView.js
+++ client/components/CartView.js
@@ -1,48 +1,39 @@
-
-import React, { Component, useState } from "react";
-import { connect } from "react-redux";
+import React, { Component, useState } from 'react';
+import { connect } from 'react-redux';
 import {
-  fetchCartProducts,
-  updateCartProduct,
-  fetchCheckout,
-} from "../store/cart";
-import { me } from "../store/auth";
+	fetchCartProducts,
+	updateCartProduct,
+	fetchCheckout,
+} from '../store/cart';
+import { me } from '../store/auth';
 
 class Cart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      numOfOrders: this.props.pastOrders.length,
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			numOfOrders: 0,
+		};
 
-    this.handleCheckout = this.handleCheckout.bind(this);
-  }
-  componentDidMount() {
-    try {
-      this.props.getCartProducts(this.props.userId);
-    } catch (error) {
-      console.log(error);
-    }
-  }
+		this.handleCheckout = this.handleCheckout.bind(this);
+	}
+	componentDidMount() {
+		try {
+			this.props.getCartProducts(this.props.userId);
+		} catch (error) {
+			console.log(error);
+		}
+	}
 
-  async handleCheckout(user, cart) {
-    await this.props.checkout(user, cart);
-    await this.props.auth();
-    this.setState({
-      numOfOrders: this.props.pastOrders.length,
-    });
-  }
+	async handleCheckout(user, cart) {
+		await this.props.checkout(user, cart);
+		await this.props.auth();
+		this.setState({
+			numOfOrders: this.props.pastOrders.length,
+		});
+	}
 
-  render() {
-    		let qty = [0, 1, 2, 3, 4, 5, 6, 7];
-    let addedProducts = this.props.cartProducts.length ? (
-      this.props.cartProducts.map((product) => {
-        return (
-          <li className="collection-product avatar" key={product.id}>
-            <div className="product-img">
-              <img src={product.img} className="" />
-            </div>
-
+	render() {
+		let qty = [0, 1, 2, 3, 4, 5, 6, 7];
 		let addedProducts = this.props.cartProducts.length ? (
 			this.props.cartProducts.map((product) => {
 				return (
@@ -50,7 +41,6 @@ class Cart extends Component {
 						<div className="product-img">
 							<img src={product.imageUrl} className="" />
 						</div>
-
 						<div className="product-description">
 							<span className="title">{product.name}</span>
 							<p>{product.description}</p>
@@ -93,40 +83,36 @@ class Cart extends Component {
 				<div className="cart">
 					<h5>You have ordered:</h5>
 					<ul className="collection">{addedProducts}</ul>
-<button
-            className="checkout-button"
-            onClick={() => {
-              this.handleCheckout(this.props.userId, this.props.cartProducts);
-            }}
-          >
-            Checkout
-          </button>
-          <div>
-            <button>'view previous orders: {this.state.numOfOrders}</button>
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-const mapState = (state) => ({
-  cartProducts: state.cart,
-  userId: state.auth.id,
-  pastOrders: state.auth.pastOrders,
-});
-
-const mapDispatch = (dispatch) => ({
-  getCartProducts: (userId) => dispatch(fetchCartProducts(userId)),
-  updateCartProduct: (userId, productId, action) =>
-    dispatch(updateCartProduct(userId, productId, action)),
-  checkout: (userId, cartProducts) =>
-    dispatch(fetchCheckout(userId, cartProducts)),
-  auth: () => dispatch(me()),
+					<button
+						className="checkout-button"
+						onClick={() => {
+							this.handleCheckout(this.props.userId, this.props.cartProducts);
+						}}
+					>
+						Checkout
+					</button>
+					<div>
+						<button>View previous orders: {this.state.numOfOrders}</button>
+					</div>
 				</div>
 			</div>
 		);
 	}
 }
+
+const mapState = (state) => ({
+	cartProducts: state.cart,
+	userId: state.auth.id,
+	pastOrders: state.auth.pastOrders,
+});
+
+const mapDispatch = (dispatch) => ({
+	getCartProducts: (userId) => dispatch(fetchCartProducts(userId)),
+	updateCartProduct: (userId, productId, action) =>
+		dispatch(updateCartProduct(userId, productId, action)),
+	checkout: (userId, cartProducts) =>
+		dispatch(fetchCheckout(userId, cartProducts)),
+	auth: () => dispatch(me()),
+});
 
 export default connect(mapState, mapDispatch)(Cart);
