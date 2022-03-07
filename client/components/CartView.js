665dea9f948a467577658b64a9d32c2cb094207b
joshua constine <joshua.constine97@gmail.com>
2022-03-07 15:06:12 -0600

    added detch cart to update on click

--- client/components/CartView.js
+++ client/components/CartView.js
@@ -1,31 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
-import { fetchCheckout } from '../store/cart';
-import ProductCheckoutCard from './Cards/ProductCheckoutCard'
-
+import { fetchCart, fetchCheckout } from "../store/cart";
+import ProductCheckoutCard from "./Cards/ProductCheckoutCard";
+import { me } from "../store/auth";
 
 export const Cart = () => {
-	const cart = useSelector((state) => state.cart.products)
+  const cart = useSelector((state) => state.cart.products);
 
-	const dispatch = useDispatch()
-	const history = useHistory()
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-	const handleCheckout = () => {
-		alert("Proceed with checkout?");
-		dispatch(fetchCheckout())
-	}
+  const handleCheckout = () => {
+    alert("Proceed with checkout?");
+    dispatch(fetchCheckout())
+      .then(() => {
+        dispatch(fetchCart());
+      });
+  };
 
-	return(
-		<div>
-			<h3>Current Cart</h3>
-			{!cart.length ? <h4>No items in cart</h4> :
-				cart.map(product => <ProductCheckoutCard product={product} key={product.id} />)
-			}
-			<button onClick={handleCheckout}>Checkout</button>
-			<button onClick={() => {history.push('/orderHistory')}}>Previous Orders</button>
-		</div>
-	)
-}
+  return (
+    <div>
+      <h3>Current Cart</h3>
+      {!cart.length ? (
+        <h4>No items in cart</h4>
+      ) : (
+        cart.map((product) => (
+          <ProductCheckoutCard product={product} key={product.id} />
+        ))
+      )}
+      <button onClick={handleCheckout}>Checkout</button>
+      <button
+        onClick={() => {
+          history.push("/orderHistory");
+        }}
+      >
+        Previous Orders
+      </button>
+    </div>
+  );
+};
 
 export default Cart;
