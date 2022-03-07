a0de7bd005f630b53f6055295647080746cca24b
AndrewRuesga <95383524+AndrewRuesga@users.noreply.github.com>
2022-03-07 16:29:05 -0600

    Stripe Feature- Frnt&Bckend

--- client/components/CartView.js
+++ client/components/CartView.js
@@ -1,31 +1,34 @@
-import React from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
-import { fetchCheckout } from '../store/cart';
-import ProductCheckoutCard from './Cards/ProductCheckoutCard'
-
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import ProductCheckoutCard from './Cards/ProductCheckoutCard';
+import { makePayment } from './Checkout/makePayment';
 
 export const Cart = () => {
-	const cart = useSelector((state) => state.cart.products)
+	const cart = useSelector((state) => state.cart.products);
 
-	const dispatch = useDispatch()
-	const history = useHistory()
+	const history = useHistory();
 
-	const handleCheckout = () => {
-		alert("Proceed with checkout?");
-		dispatch(fetchCheckout())
-	}
-
-	return(
+	return (
 		<div>
 			<h3>Current Cart</h3>
-			{!cart.length ? <h4>No items in cart</h4> :
-				cart.map(product => <ProductCheckoutCard product={product} key={product.id} />)
-			}
-			<button onClick={handleCheckout}>Checkout</button>
-			<button onClick={() => {history.push('/orderHistory')}}>Previous Orders</button>
+			{!cart.length ? (
+				<h4>No items in cart</h4>
+			) : (
+				cart.map((product) => (
+					<ProductCheckoutCard product={product} key={product.id} />
+				))
+			)}
+			{makePayment()}
+			<button
+				onClick={() => {
+					history.push('/orderHistory');
+				}}
+			>
+				Previous Orders
+			</button>
 		</div>
-	)
-}
+	);
+};
 
 export default Cart;
