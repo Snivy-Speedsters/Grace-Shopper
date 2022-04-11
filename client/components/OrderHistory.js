7d593acabd028014b071daabe4085d24ee7c9535
AndrewRuesga <95383524+AndrewRuesga@users.noreply.github.com>
2022-04-11 11:43:21 -0500

    past orders mui

--- client/components/OrderHistory.js
+++ client/components/OrderHistory.js
@@ -2,7 +2,18 @@ import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchOrders } from '../store/orders';
 import { Link } from 'react-router-dom';
-
+import {
+	Card,
+	CardMedia,
+	CardContent,
+	Typography,
+	CardActionArea,
+	Box,
+	Chip,
+	IconButton,
+	Container,
+	Grid,
+} from '@mui/material';
 const OrderHistory = () => {
 	const orders = useSelector((state) => state.orders);
 
@@ -14,30 +25,42 @@ const OrderHistory = () => {
 
 	const allOrders = orders.map((order) => {
 		return order.cart.map((product) => {
+			console.log(product);
 			return (
-				<li className="collection-product avatar" key={product.id}>
-					<div className="product-img">
-						<img src={product.imageUrl} className="" />
-					</div>
-					<div className="product-description">
-						<span className="title">{product.name}</span>
-						<p>{product.description}</p>
-						<p>
-							<b>Price: {product.price}$</b>
-						</p>
-					</div>
-				</li>
+				<Card
+					container
+					spacing={2}
+					className="collection-product avatar"
+					style={{ marginRight: '2rem', marginBottom: '2rem' }}
+					key={product.id}
+				>
+					<CardMedia
+						component="img"
+						height="300"
+						image={product.imageUrl}
+						className=""
+					/>
+					<CardContent>
+						<Typography gutterBottom variant="h5" component="div">
+							{product.name}
+						</Typography>
+						<Typography gutterBottom variant="body2" color="text.secondary">
+							{product.description}
+						</Typography>
+						<Typography gutterBottom variant="h5" component="div">
+							${product.price}
+						</Typography>
+					</CardContent>
+				</Card>
 			);
 		});
 	});
 
 	return (
-		<div className="container">
-			<div className="cart">
-				<h5>You have ordered:</h5>
-				<ul className="collection">{allOrders}</ul>
-			</div>
-		</div>
+		<Container>
+			<h1>You have ordered:</h1>
+			<Grid container>{allOrders}</Grid>
+		</Container>
 	);
 };
 
